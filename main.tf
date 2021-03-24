7df2dc8be3cf22033ab64b6c24ca532b12af05f8
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-24 14:07:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,53 @@ terraform {
 ############################################################
 # CREATE BASIC ELEMENTS FOR APPDYNAMICS
 ############################################################
+
+
+
+
+
+############################################################
+# DEPLOY CRDS AND CUSTOM ELEMENTS
+############################################################
+resource "helm_release" "appd-crd" {
+  name       = "appd-crd"
+
+  chart      = "${path.module}/helm/"
+  
+  namespace  = var.appd_namespace
+  
+  set {
+    name  = "appd_account_name"
+    value = var.appd_account_name
+  }
+  
+  set {
+    name  = "appname"
+    value = var.app_name
+  }
+  
+  set {
+    name  = "proxy_url"
+    value = var.proxy_url
+  }
+  
+  set {
+    name  = "ns_to_monitor"
+    value = "{${join(",", var.appd_ns_to_monitor)}}"
+  }
+  
+  set {
+    name  = "ns_to_instrument"
+    value = var.ns_to_instrument
+  }
+  
+  set {
+    name  = "registry"
+    value = var.registry
+  }
+  
+  set {
+    name  = "appd_global_account"
+    value = var.appd_global_account
+  }
+}
